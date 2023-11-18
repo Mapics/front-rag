a4429550348f934f22bde388478e9d090b07c62f
M4XAW <m.picault@ecole-ipssi.net>
2023-11-18 17:19:00 +0100

    fix date price car

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -22,8 +22,22 @@ export default function Cart() {
     localStorage.setItem("cart", JSON.stringify(updatedCart));
   };
 
+  const calculateItemTotal = (item) => {
+    // Calculer le nombre de jours entre la date de début et la date de fin
+    const startDate = new Date(item.dateStart);
+    const endDate = new Date(item.dateEnd);
+    const numberOfDays = Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24));
+
+    // Calculer le coût total pour cet article
+    const itemTotal = parseFloat(item.prix) * numberOfDays;
+    
+    // Retourner le coût total avec le format fixé à 2 décimales
+    return itemTotal.toFixed(2);
+  };
+
   const calculateTotal = () => {
-    return cartItems.reduce((total, item) => total + parseFloat(item.prix), 0).toFixed(2);
+    // Calculer le total général en additionnant le coût total de chaque article
+    return cartItems.reduce((total, item) => total + parseFloat(calculateItemTotal(item)), 0).toFixed(2);
   };
 
   const buyCart = () => {
@@ -68,9 +82,7 @@ export default function Cart() {
             {cartItems.map((item, index) => (
               <p className="cartTotalProduct">
                 <p className="title">{item.titre}</p>
-                <p className="price" key={index}>
-                  {item.prix}€
-                </p>
+                <p className="price">{`${item.prix}€/jour`}</p>
               </p>
             ))}
           </div>
